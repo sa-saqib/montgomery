caab51b5a08bee473325ea6987520ba2e31b03ff
Saqib <saqi.galahad@gmail.com>
2025-06-13 15:43:55 +0530

    Clean up

--- into8bits.py
+++ into8bits.py
@@ -7,7 +7,6 @@ def into8(hex_string):
     result = [f"{byte:02x}" for byte in reversed(byte_array)] 
     return result
 
- 
 # input = "44efb931608c078de7e94c4acd313c4ecc3da954e48b0fd380590dacdcbe843e"
 # output = into8(input)
 # print(output)
